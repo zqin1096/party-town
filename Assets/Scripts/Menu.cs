a151053b4cf85b3bc2349d1effb784ccd5981efe
Zhaoyin Qin <zqin1096@gmail.com>
2020-02-18 14:59:23 -0800

    Setup project

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Menu : MonoBehaviourPunCallbacks
+{
+    [Header("Screens")]
+    public GameObject mainScreen;
+    public GameObject lobbyScreen;
+
+    [Header("Main Screen")]
+    public Button createRoomButton;
+    public Button joinRoomButton;
+
+    [Header("Lobby Screen")]
+    public TextMeshProUGUI playerListText;
+    public Button startGameButton;
+
+    void Start()
+    {
+        // Disable the button when the player is not connected to the server.
+        createRoomButton.interactable = false;
+        joinRoomButton.interactable = false;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        // Re-enable the buttons.
+        createRoomButton.interactable = true;
+        joinRoomButton.interactable = true;
+    }
+
+    void SetScreen(GameObject screen)
+    {
+        mainScreen.SetActive(false);
+        lobbyScreen.SetActive(false);
+        screen.SetActive(true);
+    }
+
+    public void OnCreateRoomButton(TMP_InputField roomNameInput)
+    {
+        NetworkManager.instance.CreateRoom(roomNameInput.text);
+    }
+
+    public void OnJoinRoomButton(TMP_InputField roomNameInput)
+    {
+        NetworkManager.instance.JoinRoom(roomNameInput.text);
+    }
+
+    public void OnPlayerNameUpdate(TMP_InputField playerNameInput)
+    {
+        PhotonNetwork.NickName = playerNameInput.text;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        SetScreen(lobbyScreen);
+        photonView.RPC("UpdateLobbyUI", RpcTarget.All);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdateLobbyUI();
+    }
+
+    [PunRPC]
+    public void UpdateLobbyUI()
+    {
+        playerListText.text = "";
+
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            playerListText.text += player.NickName + "\n";
+        }
+
+        if (PhotonNetwork.IsMasterClient)
+            startGameButton.interactable = true;
+        else
+            startGameButton.interactable = false;
+    }
+
+    public void OnLeaveLobbyButton()
+    {
+        PhotonNetwork.LeaveRoom();
+        SetScreen(mainScreen);
+    }
+
+    public void OnStartGameButton()
+    {
+        NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, "Game");
+    }
+}
---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // NetworkManager instance (singleton).
+    public static NetworkManager instance;
+
+    // Awake is used to initialize any variable or game state before the game starts.
+    // Awake is called only once during the lifetime of the script instance.
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            // Check if an instance already exists (game scene back to the menu scene).
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            // Set the instance to be the script.
+            instance = this;
+            // gameObject is what the script is on.
+            // Do not destroy the target Object when loading a new Scene.
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to the master server");
+        // CreateRoom("testRoom");
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created room: " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    public void CreateRoom(string roomName)
+    {
+        PhotonNetwork.CreateRoom(roomName);
+    }
+
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    // This function will be used as an RPC. When the host starts the game, everyone in the
+    // room will call this function.
+    [PunRPC]
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+}
