e7d81d55a499b3c38db115ea761663550a57dfdb
Elden <eldeniyenden@gmail.com>
2020-04-28 10:20:24 -0700

    docs: add video and documentation to the web page

--- scripts/build.js
+++ scripts/build.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const gulp = require('gulp');
 const path = require('path');
 
-const buildLog = buildLogger('trojan-blitz');
+const buildLog = buildLogger('[trojan-blitz]');
 
 const paths = {
   docs: path.resolve(__dirname, '../docs'),
@@ -49,31 +49,104 @@ gulp.task('modify-html', () => {
   const indexHtml = fs.readFileSync(srcPath).toString();
   const $ = cheerio.load(indexHtml);
 
+  const scriptHtml = `
+<script>
+  window.onload = function handleOnLoad() {
+    if (window.screen.availWidth <= 480) {
+      const body = document.getElementsByTagName('body')[0]
+      body.classList.add('mobile');
+    }
+  }
+  window.onresize = function handleOnResize() {
+    const body = document.getElementsByTagName('body')[0]
+    if (window.screen.availWidth <= 480) {
+      const body = document.getElementsByTagName('body')[0]
+      body.classList.add('mobile');
+    } else {
+      body.classList.remove('mobile');
+    }
+  }
+</script>
+`;
   const styleHtml = `
 <style>
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+  body.mobile {
+    font-size: 5vw;
+  }
+  #wrap {
+    margin: 80px auto 0;
+    width: 960px;
+  }
+  @media (max-width: 480px) {
+    body {
+      font-size: 15vw;
+    }
+  }
+  .h2 {
+    margin-top: 0.5em;
+    margin-bottom: 0.2em;
+  }
+  .warn {
+    color: #767676;
+  }
   .webgl-content {
     position: static;
     transform: none;
   }
+  #desc {
+    margin-top: 2.6em;
+  }
+  .ref {
+    margin-top: 0.6em;
+  }
 </style>
+`;
+
+  const warnHtml = `
+<p class="warn">
+  This application is designed to run in devices that support WebGL, with the viewport ratio ideally 2:1. Position your device horizontally so that width exceeds height.
+  Note also that the game is served on a Photon public cloud. This may no longer be available in the future.
+</p>
 `;
 
   const descHtml = `
 <div id="desc">
-  <h2>
-    123
+  <h2 class="h2">
+    Trojan Blitz
   </h2>
   <p>
-    power
+    A highly versatile, fast-paced, deck-building card game for the mobile platform. Intuitive play with deck building strategy to compete in battles on the clock. Players choose their character at the start of a match from a randomly generated selection, and then play using the deck they have customized with their own cards.
+  </p>
+  <h2 class="h2">
+    Gameplay
+  </h2>
+  <div style="width:100%;height:0px;position:relative;padding-bottom:48.387%;"><iframe src="https://streamable.com/e/8qizpb" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+  <h2 class="h2">
+    Multiplayer Game - System Design
+  </h2>
+  <p>
+    Trojan Blitz, created with Unity, written in C#, is a multiplayer game application, that is designed to run in mobile devices. Using remote procedure calls (RPCs), it propagates a state of the application to player nodes in the network. It might as well have been architected in such a way that it only broadcasts "action" to the participating nodes. Other than multi-agent element, Trojan Blitz has an architecture typically expected of game applications. GameManager, Character, Card, Player are fundamental classes that make up this game.
+  </p>
+  <p class="ref">
+    <a href="https://github.com/zqin1096/party-town">Github</a>
+  </p>
+  <p class="h2">
+    This project is initiated as the course assignment of CSCI599 - Special Topics (Social Mobile Games), at the University of Southern California in Spring 2020.
   </p>
 </div>
 `;
-  $('body').prepend('<div id="wrap"></div>');
-  $('head').append(styleHtml);
 
+  $('head').append(styleHtml);
+  $('head').append(scriptHtml);
+  $('body').prepend('<div id="wrap"></div>');
   const webglContent = $('.webgl-content');
   webglContent.append(descHtml);
 
+  $('#wrap').prepend(warnHtml);
   $('#wrap').append(webglContent);
   const nextHtml = $.html();
 
